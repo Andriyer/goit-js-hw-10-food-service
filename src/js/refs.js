9cde2e61da14746c3e12ee3faf23301714a31bd1
Andriyer <83455102+Andriyer@users.noreply.github.com>
2021-10-24 18:27:44 +0000

    Deploying to gh-pages from @ Andriyer/goit-js-hw-10-food-service@a7d3e4c7ebd539a584b4326143f3c1bbaa681bb8 🚀

--- src/js/refs.js
+++
@@ -1,7 +0,0 @@
-const refs = {
-    bodyEl: document.querySelector('body'),
-    themeSwitchEL: document.querySelector('.theme-switch__toggle'),
-    menuEL: document.querySelector('.js-menu'),
-  };
-  
-  export const { bodyEl, themeSwitchEL, menuEL } = refs;
--- src/js/theme.js
+++
@@ -1,6 +0,0 @@
-const theme = {
-    LIGHT: 'light-theme',
-    DARK: 'dark-theme',
-  };
-  
-  export const { LIGHT, DARK } = theme;
